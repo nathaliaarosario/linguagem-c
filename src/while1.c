8e3f021fa942b9c1f910950cf57654852f79fe88
Nathalia Rosario <nathaliaalvesrosario@gmail.com>
2025-09-09 17:21:33 -0300

    Laço:Primeiro exemplo de traço while

---
+++ src/while1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    system("clear");
+    int i = 1; //inicio da variavel
+    while ( i <= 10 ){ //condiçõa para execução
+        printf("%d\n",i); 
+        i++;
+    }
+    return 0;
+}
